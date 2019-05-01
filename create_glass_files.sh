cc0a752a533ca63c5e1347e653468b5c8ee698d7
Josh Borrow <joshua.borrow@durham.ac.uk>
2019-05-01 10:14:22 +0100

    Added script to create glass files

---
+++ create_glass_files.sh
@@ -0,0 +1,21 @@
+# This script creates glass files from the snapshots in the directory.
+
+mkdir glass_files
+
+for hydro in gadget2 minimal pressure-energy anarchy-pu gizmo-mfm gizmo-mfv
+do
+    for kernel in cubic-spline quintic-spline wendland-C2
+    do
+        mkdir -p glass_files/$hydro/$kernel
+
+        for npart in 16 32 64 128
+        do
+            # Find last snapshot
+            x="$(ls glass_$npart/$kernel/$scheme | egrep -o '00([0-9]+)' | tail -1)"
+            filename=glass_$npart/$kernel/$scheme/uniformBox_$x.hdf5
+
+            cp glass_$npart/$kernel/$scheme/$filename glass_files/$hydro/$kernel/glassCube_$npart.hdf5
+        done
+    done
+done
+
